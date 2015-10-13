9d24181df4ef748e5dcfe154d6ddca3bb9822a0d
a923887092 <923887092@qq.com>
2015-10-13 09:31:22 +0800

    yyyyy

--- app/src/main/java/com/gwm/sweethouse/GuideActivity.java
+++ app/src/main/java/com/gwm/sweethouse/GuideActivity.java
@@ -2,105 +2,145 @@ package com.gwm.sweethouse;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.gwm.sweethouse.utils.PrefUtils;
-import com.viewpagerindicator.CirclePageIndicator;
 
 import java.util.ArrayList;
 
-public class GuideActivity extends Activity implements ViewPager.OnPageChangeListener {
-
-    private ViewPager vpGuide;
-    private Button btnEnterHome;
-    private ArrayList<ImageView> viewsList;
-    private static final int[] imagesArr = new int[]{R.mipmap.guide_1, R.mipmap.guide_2, R.mipmap.guide_3};
-    private CirclePageIndicator mIndicator;
-
+/**
+ * Created by Administrator on 2015/10/8 0008.
+ */
+//欢迎页面
+public class GuideActivity extends Activity {
+   private ViewPager vpGuide;
+    private LinearLayout llPointGroup;//引导圆点的父控件
+    private ArrayList<ImageView> mImageViewList;
+    private int mPointWidth;//两个圆点之间的距离
+    private View viewRedPoint;//小红点
+    private Button btnStart;
+    //获得欢迎页面的三张图片
+    private static final int[] myImageIds=new int[]{
+            R.drawable.guide1,
+            R.drawable.guide2,
+            R.drawable.guide3
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+       // requestWindowFeature(Window.FEATURE_NO_TITLE);//初始化界面时去掉标题头--但是这一步我已经在Manifest中设置过了
         setContentView(R.layout.activity_guide);
-        vpGuide = (ViewPager) findViewById(R.id.vp_guide);
-        btnEnterHome = (Button) findViewById(R.id.btn_enter_home);
-        initView();
-        vpGuide.setAdapter(new ViewPagerAdapter());
-        mIndicator = (CirclePageIndicator)findViewById(R.id.indicator);
-        mIndicator.setViewPager(vpGuide);
-        mIndicator.setOnPageChangeListener(this);
-        btnEnterHome.setOnClickListener(new View.OnClickListener() {
+        vpGuide= (ViewPager) findViewById(R.id.vp_guide);
+        viewRedPoint=findViewById(R.id.view_red_point);
+        btnStart= (Button) findViewById(R.id.button);
+        //为按钮提供监听事件
+       btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PrefUtils.setBoolean(GuideActivity.this, "guide_is_showed", true);
-
-                startActivity(new Intent(GuideActivity.this, MainActivity.class));
+                //更新sp--表示已经展示了新手引导
+                PrefUtils.setBoolean(GuideActivity.this, "is_user_guide_showed", true);
+                //跳转至主页面
+               startActivity(new Intent(GuideActivity.this, MainActivity.class));
                 finish();
             }
         });
-    }
+        llPointGroup= (LinearLayout) findViewById(R.id.ll_point_group);
+        initView();//初始化界面的方法必须写在初始化控件即findViewById后面
+        vpGuide.setAdapter(new GuideAdpter());//初始化界面后来setAdapter
+        vpGuide.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                //滑动事件
+                System.out.print("当前位置"+position+";百分比"+positionOffset
+               +"移动距离："+positionOffsetPixels );
+               int len=(int)(mPointWidth*positionOffset)+position*mPointWidth;
+                //获取当前红点的布局参数
+                RelativeLayout.LayoutParams params= (RelativeLayout.LayoutParams) viewRedPoint.getLayoutParams();
+                params.leftMargin=len;//设置左边距
+                //重新给小红点设置布局参数
+                viewRedPoint.setLayoutParams(params);
 
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            //  当前选择页面是否为最后一个页面
+                if (position==myImageIds.length-1){
+                    //显示开启新生活按钮
+                    btnStart.setVisibility(View.VISIBLE);
+                }else{
+                    btnStart.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+    //初始化引导页界面
     private void initView(){
-        viewsList = new ArrayList<>();
+        mImageViewList=new ArrayList<ImageView>();
+        for(int i=0;i<myImageIds.length;i++){
+            ImageView image=new ImageView(this);
+            //设置引导背景
+            image.setBackgroundResource(myImageIds[i]);
+            mImageViewList.add(image);
+        }
+        //初始化界面的小圆点
+        for(int i=0;i<myImageIds.length;i++){
+            View point=new View(this);
+            //设置引导背景
+            point.setBackgroundResource(R.drawable.shape_point_gray);//设置引导页小圆点
+            LinearLayout.LayoutParams layoutParams= new LinearLayout.LayoutParams(20,20);//设置引导圆点的大小
+            if(i>0){
+                layoutParams.leftMargin=10;//设置圆点的间隔
+            }
+            point.setLayoutParams(layoutParams);//设置引导圆点的大小
+            llPointGroup.addView(point);//将小圆点添加给线性布局
+            //获取视图树--监听事件结束
+            llPointGroup.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                //当layout借宿后毁掉此方法
+                @Override
+                public void onGlobalLayout() {
+                    llPointGroup.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                    mPointWidth=llPointGroup.getChildAt(1).getLeft()-llPointGroup.getChildAt(0).getLeft();
+                    System.out.print("圆点距离"+mPointWidth);
 
-        for (int i = 0; i < 3; i++){
-            ImageView image = new ImageView(this);
-            image.setImageResource(imagesArr[i]);
-            viewsList.add(image);
+                }
+            });
         }
     }
-
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-    }
-
-    @Override
-    public void onPageSelected(int position) {
-        if (position == viewsList.size() - 1){
-            btnEnterHome.setVisibility(View.VISIBLE);
-        } else {
-            btnEnterHome.setVisibility(View.INVISIBLE);
-        }
-    }
-
-    @Override
-    public void onPageScrollStateChanged(int state) {
-
-    }
-
-    class ViewPagerAdapter extends PagerAdapter{
-
+    class GuideAdpter extends PagerAdapter {
         @Override
         public int getCount() {
-            return viewsList.size();
+            return myImageIds.length;
         }
 
         @Override
         public boolean isViewFromObject(View view, Object object) {
-            return view == object;
+            return view==object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(mImageViewList.get(position));
+            return mImageViewList.get(position);
         }
 
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
             container.removeView((View) object);
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            ImageView view = viewsList.get(position);
-            container.addView(view);
-            return view;
         }
     }
 
--- app/src/main/java/com/gwm/sweethouse/SplashActivity.java
+++ app/src/main/java/com/gwm/sweethouse/SplashActivity.java
@@ -2,46 +2,58 @@ package com.gwm.sweethouse;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
-import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
 
 import com.gwm.sweethouse.utils.PrefUtils;
 
+//闪屏--动画进入
 public class SplashActivity extends Activity {
-
-    private RelativeLayout splash;
-    private AnimationSet set;
-
+    LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        splash = (RelativeLayout) findViewById(R.id.splash);
+        linearLayout= (LinearLayout) findViewById(R.id.ll_root);
         startAnim();
+    }
+    //开启动画
+    private void startAnim() {
+        AnimationSet set=new AnimationSet(false);//动画集合--旋转和缩放同时进行
+        //旋转动画
+        RotateAnimation rotate=new RotateAnimation(0,360, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        rotate.setDuration(1000);//设置动画时间
+        rotate.setFillAfter(true);//保持动画后的状态
+
+        //缩放动画
+        ScaleAnimation scale=new ScaleAnimation(0,1,0,1,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        scale.setDuration(1000);//设置动画时间
+        scale.setFillAfter(true);//保持动画状态
+
+        //渐变动画
+        AlphaAnimation alpna=new AlphaAnimation(0,1);
+        alpna.setDuration(1000);//设置动画时间
+        alpna.setFillAfter(true);//保持动画状态
+
+        set.addAnimation(rotate);
+        set.addAnimation(scale);
+        set.addAnimation(alpna);
+
+        //设置动画监听
         set.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
 
             }
-
+            //动画执行结束
             @Override
             public void onAnimationEnd(Animation animation) {
-                boolean flag = PrefUtils.getBoolean(SplashActivity.this, "guide_is_showed", false);
-                if (flag){
-                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                    finish();
-                } else {
-                    startActivity(new Intent(SplashActivity.this, GuideActivity.class));
-                    finish();
-                }
+           jumpNextPage();
             }
 
             @Override
@@ -49,27 +61,22 @@ public class SplashActivity extends Activity {
 
             }
         });
+        linearLayout.startAnimation(set);
     }
 
-    private void startAnim() {
-        set = new AnimationSet(false);
+    //跳转至下一个页面
+    private void jumpNextPage(){
+        //判断是否第一次进入过新手引导页
 
-        RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f,
-                Animation.RELATIVE_TO_SELF, 0.5f);
-        rotate.setDuration(1000);
-        rotate.setFillAfter(true);
-
-        ScaleAnimation scale = new ScaleAnimation(0, 1, 0, 1, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        scale.setFillAfter(true);
-        scale.setDuration(1000);
-
-        AlphaAnimation alpha = new AlphaAnimation(0, 1);
-        alpha.setDuration(1000);
-        alpha.setFillAfter(true);
-
-        set.addAnimation(rotate);
-        set.addAnimation(scale);
-        set.addAnimation(alpha);
-        splash.startAnimation(set);
+        //默认不是第一次进入
+        boolean userGuide= PrefUtils.getBoolean(this, "is_user_guide_show", false);
+        if(!userGuide){
+            startActivity(new Intent(SplashActivity.this,GuideActivity.class));
+        }else{
+            startActivity(new Intent(SplashActivity.this,MainActivity.class));
+        }
+        finish();
     }
+
+
 }
